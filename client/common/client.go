0640140a60877d1780f56b6126207f8f6d596f67
ignacioregazzoli <ignacioregazzoli@gmail.com>
2025-03-22 09:52:17 -0300

    Revert "Update log for tests"
    This reverts commit 9da0edac9f480f5e627f9de2e247eecd493218e1.

--- client/common/client.go
+++ client/common/client.go
@@ -154,8 +154,7 @@ func (c *Client) sendBatches() error {
 	if err != nil {
 		return fmt.Errorf("error consulting winners: %w", err)
 	}
-	log.Infof("action: consulta_ganadores | result: success | cant_ganadores: %d | source: client%s", len(winners), c.config.ID)
-
+	log.Infof("action: consulta_ganadores | result: success | cant_ganadores: %d", len(winners))
 
 	return nil
 }
